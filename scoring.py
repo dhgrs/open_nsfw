35b05baed0380a34918dc2e74af74b828efedc8f
dhgrs <d.higurashi@gmail.com>
2017-10-18 17:58:38 +0900

    fix a bug on a machine with GPU

--- scoring.py
+++ scoring.py
@@ -16,8 +16,9 @@ def score(path):
     img = np.asarray(img).astype(np.float32) / 255
     h, w, _ = img.shape
     img = np.expand_dims(img.transpose((2, 0, 1)), 0)
-    x, = model(inputs={'data': img}, outputs=['eltwise_stage3_block2'],
-               disable=['pool', 'fc_nsfw', 'prob'])
+    with chainer.using_config('use_cudnn', 'never'):
+        x, = model(inputs={'data': img}, outputs=['eltwise_stage3_block2'],
+                   disable=['pool', 'fc_nsfw', 'prob'])
 
     W = model.fc_nsfw.W.data.reshape(2, 1024, 1, 1)
     b = model.fc_nsfw.b.data
@@ -28,5 +29,6 @@ def score(path):
         np.clip((img[0] * score.data).transpose((1, 2, 0)) * 255, 0, 255),
         dtype=np.uint8)
     Image.fromarray(scored_img).save('result.png')
+
 
 score(sys.argv[1])
