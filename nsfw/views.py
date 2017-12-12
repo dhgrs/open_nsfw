bf0352a456883d270484d0ae6d2362ec0f49b019
dhgrs <d.higurashi@gmail.com>
2017-12-12 21:25:42 +0900

    add reshape
    for memory reduction

--- nsfw/views.py
+++ nsfw/views.py
@@ -3,6 +3,7 @@ from django.template import RequestContext
 from django.views.decorators.csrf import csrf_protect
 from django.core.cache import cache
 import chainer
+from chainer import functions as F
 from chainer.links import caffe
 from PIL import Image
 import numpy as np
@@ -29,10 +30,12 @@ class NSFW(object):
             img = np.asarray(img_org).astype(np.float32) / 255
             h, w, _ = img.shape
             img = np.expand_dims(img.transpose((2, 0, 1)), 0)
+
             with chainer.using_config('use_cudnn', 'never'):
-                x, = model(inputs={'data': img},
-                           outputs=['eltwise_stage3_block2'],
-                           disable=['pool', 'fc_nsfw', 'prob'])
+                x, = model(
+                    inputs={'data': F.resize_images(img, (224, 224))},
+                    outputs=['eltwise_stage3_block2'],
+                    disable=['pool', 'fc_nsfw', 'prob'])
 
             W = model.fc_nsfw.W.data.reshape(2, 1024, 1, 1)
             b = model.fc_nsfw.b.data
